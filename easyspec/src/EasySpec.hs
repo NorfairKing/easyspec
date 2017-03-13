f1b2b47c89d341491ef4b3c7ff5939e0a95af6a6
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2017-03-13 20:50:51 +0100

    Little refactoring

--- easyspec/src/EasySpec.hs
+++ easyspec/src/EasySpec.hs
@@ -31,61 +31,67 @@ easyspec = do
 dispatch
     :: (MonadIO m, MonadReader Settings m)
     => Dispatch -> m ()
-dispatch (DispatchDiscover dsets@DiscoverSettings {..}) = do
-    ids <- getIds dsets
+dispatch (DispatchDiscover ds) = do
+    ids <- getIds ds
+    runEasySpec ds ids
+
+runEasySpec
+    :: MonadIO m
+    => DiscoverSettings -> [GHC.Id] -> m ()
+runEasySpec ds ids =
     liftIO $
-        runGhc (Just libdir) $
+    runGhc (Just libdir) $
             -- Make the quickspec signature code
-         do
-            quickspecSig <- createQuickspecSig ids
             -- For some reason I have to restart the session
-            initGhcMonad (Just libdir)
-            dflags <- getSessionDynFlags
-            let intdfflags =
-                    (prepareFlags dflags)
-                    {hscTarget = HscInterpreted, ghcLink = LinkInMemory}
-            setSessionDynFlags intdfflags
-            -- For some reason this star is necessary:
-            -- https://stackoverflow.com/questions/12790341/haskell-ghc-dynamic-compliation-only-works-on-first-compile
-            -- https://mail.haskell.org/pipermail/glasgow-haskell-users/2011-October/021009.html
-            target <- guessTarget ("*" ++ toFilePath setDiscFile) Nothing
-            printO target
-            setTargets [target]
-            load LoadAllTargets
-            let imp = IIDecl . GHC.simpleImportDecl . GHC.mkModuleName
-            let qsModules =
-                    [ imp "Test.QuickSpec"
-                    , imp "Test.QuickSpec.Signature"
-                    , imp "Prelude"
-                    , IIModule $ mkModuleName "Monomorphic"
-                    ]
-            setContext qsModules
-            liftIO $ putStrLn quickspecSig
-            getBindings >>= printO
-            void $
-                execStmt
-                    (unwords ["quickSpec", "(", quickspecSig, ")"])
-                    execOptions
+     do
+        initGhcMonad (Just libdir)
+        dflags <- getSessionDynFlags
+        let intdfflags =
+                (prepareFlags dflags)
+                {hscTarget = HscInterpreted, ghcLink = LinkInMemory}
+        setSessionDynFlags intdfflags
+            -- This star is necessary so GHC uses the sources instead of the already compiled .o files.
+            -- See these:
+            -- - https://stackoverflow.com/questions/12790341/haskell-ghc-dynamic-compliation-only-works-on-first-compile
+            -- - https://mail.haskell.org/pipermail/glasgow-haskell-users/2011-October/021009.html
+        target <- guessTarget ("*" ++ toFilePath (setDiscFile ds)) Nothing
+        setTargets [target]
+        load LoadAllTargets
+        let imp = IIDecl . GHC.simpleImportDecl . GHC.mkModuleName
+        let qsModules =
+                [ imp "Test.QuickSpec"
+                , imp "Test.QuickSpec.Signature"
+                , imp "Prelude"
+                , IIModule $ getTargetModName ds
+                ]
+        setContext qsModules
+        getBindings >>= printO
+        quickspecSig <- createQuickspecSig ids
+        liftIO $ putStrLn quickspecSig
+        void $
+            execStmt (unwords ["quickSpec", "(", quickspecSig, ")"]) execOptions
 
 prepareFlags :: DynFlags -> DynFlags
 prepareFlags dflags = foldl xopt_set dflags [Cpp, ImplicitPrelude, MagicHash]
 
+getTargetModName :: DiscoverSettings -> ModuleName
+getTargetModName =
+    mkModuleName . dropExtension . toFilePath . filename . setDiscFile
+
 getIds
     :: MonadIO m
     => DiscoverSettings -> m [GHC.Id]
-getIds DiscoverSettings {..} =
+getIds ds@DiscoverSettings {..} =
     liftIO $
     runGhc (Just libdir) $ do
         dflags <- getSessionDynFlags
         let compdflags = prepareFlags dflags
         setSessionDynFlags compdflags
         target <- guessTarget (toFilePath setDiscFile) Nothing
-        printO target
         setTargets [target]
         load LoadAllTargets
                     -- Doesn't work in a project, only in top-level modules
-        let modname =
-                mkModuleName $ dropExtension $ toFilePath $ filename setDiscFile
+        let modname = getTargetModName ds
         printO modname
         modSum <- getModSummary modname
         parsedModule <- parseModule modSum

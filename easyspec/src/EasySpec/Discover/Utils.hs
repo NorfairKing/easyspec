5e94e6d3c8961c0dd016b8eaf3883dd31326a26f
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2017-03-15 12:48:29 +0100

    Single-variable types in signature should now work

--- easyspec/src/EasySpec/Discover/Utils.hs
+++ easyspec/src/EasySpec/Discover/Utils.hs
@@ -9,7 +9,11 @@ import System.FilePath
 import DynFlags hiding (Settings)
 import GHC
 import GHC.LanguageExtensions
+import Name
 import Outputable
+import TyCoRep
+import TyCon
+import Type
 import Var
 
 import EasySpec.OptParse
@@ -39,18 +43,54 @@ createQuickspecSig
     :: GhcMonad m
     => [GHC.Id] -> m String
 createQuickspecSig ids = do
-    constantExprs <- mapM idConstant ids
-    let constantList = intercalate ", " constantExprs
-    pure $
-        "signature [" ++
-        constantList ++ ", " ++ "vars [\"a\"] (undefined :: Char)]"
+    componentExprs <- mapM idSigComponent ids
+    let componentList = intercalate ", " componentExprs
+    pure $ "signature [" ++ componentList ++ "]"
 
-idConstant
+idSigComponent
     :: GhcMonad m
     => GHC.Id -> m String
-idConstant i = do
+idSigComponent i = do
     name <- showGHC $ Var.varName i
-    pure $ unwords ["fun1", show name, name]
+    typs <- showGHC $ Var.varType i
+    let typ = Var.varType i
+    let Comp compar compty = comp typ
+    liftIO $ print (name, compty, typs)
+    pure $
+        unwords
+            ["fun" ++ show compar, show name, "(" ++ name, "::", compty ++ ")"]
+
+data Comp = Comp
+    { compAr :: Int
+    , compNm :: String
+    } deriving (Show, Eq)
+
+comp :: GHC.Type -> Comp
+comp = go
+  where
+    go t =
+        case t of
+            TyVarTy _ -> Comp 0 "A" -- TODO allow for other type variables too.
+            AppTy t1 t2 ->
+                let Comp _ vn1 = go t1
+                    Comp _ vn2 = go t2
+                in Comp 0 $ unwords [vn1, vn2]
+            TyConApp tc kots ->
+                let cs = map go kots
+                in Comp 0 $ unwords $ showName (tyConName tc) : map compNm cs
+            ForAllTy _ t'
+                -- No idea why this is necessary here...
+             ->
+                case splitFunTy_maybe t of
+                    Nothing -> go t'
+                    Just (tf, tt) ->
+                        let Comp _ vn1 = go tf
+                            Comp _ vn2 = go tt
+                        in Comp 1 $ unwords [vn1, "->", vn2]
+            _ -> error "not implemented yet."
+
+showName :: Name -> String
+showName = occNameString . Name.nameOccName
 
 showGHC
     :: (GhcMonad m, Outputable a)

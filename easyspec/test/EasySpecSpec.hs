b1af3221a95c2038b7073f4f31d03af802623463
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2017-03-15 09:27:45 +0100

    Failing tests for the reverse example

--- easyspec/test/EasySpecSpec.hs
+++ easyspec/test/EasySpecSpec.hs
@@ -10,6 +10,20 @@ import EasySpec
 
 spec :: Spec
 spec =
-    describe "examples" $
-    it "works on Monomorphic.hs" $
-    withArgs ["discover", "../examples/Monomorphic.hs"] easyspec
+    describe "examples" $ do
+        exampleDir <- runIO $ resolveDir' "../examples"
+        forSourceFilesInDir
+            exampleDir
+            (\f -> unwords ["works on", toFilePath f])
+            (\f -> withArgs ["discover", toFilePath f] easyspec)
+
+forSourceFilesInDir
+    :: Example a
+    => Path Abs Dir
+    -> (Path Abs File -> String)
+    -> (Path Abs File -> a)
+    -> SpecWith (Arg a)
+forSourceFilesInDir dir itfunc func = do
+    fs <-
+        runIO $ (filter ((== ".hs") . fileExtension) . snd) <$> listDirRecur dir
+    forM_ fs $ \f -> it (itfunc f) $ func f

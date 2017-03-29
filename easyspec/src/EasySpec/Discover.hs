623584fbc63115fbb5df042c802154af3f9318d9
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2017-03-29 03:35:41 +0200

    Started on a compatibilyt layer. Looks like this is going to work

--- easyspec/src/EasySpec/Discover.hs
+++ easyspec/src/EasySpec/Discover.hs
@@ -4,6 +4,8 @@ module EasySpec.Discover where
 
 import Import
 
+import Language.Haskell.Exts.Pretty
+
 import EasySpec.OptParse
 
 import EasySpec.Discover.GHC
@@ -12,4 +14,5 @@ import EasySpec.Discover.Gather
 discover :: (MonadIO m, MonadReader Settings m) => DiscoverSettings -> m ()
 discover ds = do
     ids <- getIds ds
+    liftIO $ mapM_ (putStrLn . prettyPrint . easyType . toEasyId) ids
     runEasySpec ds ids
--- easyspec/src/EasySpec/Discover/Gather.hs
+++ easyspec/src/EasySpec/Discover/Gather.hs
@@ -10,6 +10,12 @@ import GHC.Paths (libdir)
 import OccName
 import RdrName
 import TcRnTypes
+import TyCoRep
+import TyCon
+import Type
+import Var
+
+import Language.Haskell.Exts.Syntax as H
 
 import EasySpec.OptParse
 
@@ -41,3 +47,40 @@ getIds ds@DiscoverSettings {..} =
                         Just (AnId i) -> Just i
                         Just _ -> Nothing
                         Nothing -> Nothing
+
+data EasyId = EasyId
+    { easyName :: EasyName
+    , easyType :: EasyType
+    } deriving (Show, Eq)
+
+type EasyName = H.Name ()
+
+type EasyType = H.Type ()
+
+toEasyId :: GHC.Id -> EasyId
+toEasyId i =
+    EasyId
+    { easyName = toEasyName $ Var.varName i
+    , easyType = toEasyType $ Var.varType i
+    }
+
+toEasyName :: Monoid a => GHC.Name -> H.Name a
+toEasyName n = Ident mempty $ showName n
+
+toEasyType :: Monoid a => GHC.Type -> H.Type a
+toEasyType ty =
+    case splitFunTy_maybe ty of
+        Nothing -> go ty
+        Just (tf, tt) -> H.TyFun mempty (toEasyType tf) (toEasyType tt)
+  where
+    go t =
+        case t of
+            TyVarTy i -> TyVar mempty $ toEasyName $ Var.varName i
+            AppTy t1 t2 -> TyApp mempty (toEasyType t1) (toEasyType t2)
+            TyConApp tc kots ->
+                foldl
+                    (TyApp mempty)
+                    (TyCon mempty $ UnQual mempty $ toEasyName $ tyConName tc)
+                    (map toEasyType kots)
+            ForAllTy _ t' -> TyForall mempty Nothing Nothing $ toEasyType t'
+            _ -> error "Not implemented yet"

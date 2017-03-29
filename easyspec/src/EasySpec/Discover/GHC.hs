5a213efcd3ec5d4b68a9e1194380ec304bc16603
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2017-03-29 02:47:59 +0200

    Tiny refactor

---
+++ easyspec/src/EasySpec/Discover/GHC.hs
@@ -0,0 +1,64 @@
+module EasySpec.Discover.GHC where
+
+import Import
+
+import DynFlags hiding (Settings)
+import GHC
+import GHC.LanguageExtensions
+import GHC.Paths (libdir)
+
+import EasySpec.OptParse
+
+import EasySpec.Discover.Utils
+
+runEasySpec :: MonadIO m => DiscoverSettings -> [GHC.Id] -> m ()
+runEasySpec ds ids =
+    liftIO $
+    runGhc (Just libdir) $
+            -- Make the quickspec signature code
+            -- For some reason I have to restart the session
+     do
+        initGhcMonad (Just libdir)
+        dflags <- getSessionDynFlags
+        let intdfflags =
+                (addTypeClassExts $ prepareFlags dflags)
+                {hscTarget = HscInterpreted, ghcLink = LinkInMemory}
+        setDFlagsNoLinking intdfflags
+            -- This star is necessary so GHC uses the sources instead of the already compiled .o files.
+            -- See these:
+            -- - https://stackoverflow.com/questions/12790341/haskell-ghc-dynamic-compliation-only-works-on-first-compile
+            -- - https://mail.haskell.org/pipermail/glasgow-haskell-users/2011-October/021009.html
+        target <- guessTarget ("*" ++ toFilePath (setDiscFile ds)) Nothing
+        setTargets [target]
+        loadSuccessfully LoadAllTargets
+        let imp = IIDecl . GHC.simpleImportDecl . GHC.mkModuleName
+        let qsModules =
+                [ imp "QuickSpec"
+                , imp "QuickSpec.Signature"
+                , imp "Prelude"
+                , IIModule $ getTargetModName ds
+                ]
+        setContext qsModules
+        getBindings >>= printO
+        quickspecSig <- createQuickspecSig ids
+        liftIO $ putStrLn quickspecSig
+        let declaretc =
+                unlines
+                    [ "let typeclass :: (c => a) -> Dict c -> a"
+                    , "    typeclass x Dict = x"
+                    ]
+        void $ execStmt declaretc execOptions
+        void $
+            execStmt (unwords ["quickSpec", "(", quickspecSig, ")"]) execOptions
+
+addTypeClassExts :: DynFlags -> DynFlags
+addTypeClassExts dflags =
+    foldl
+        xopt_set
+        dflags
+        [ ScopedTypeVariables
+        , ConstraintKinds
+        , RankNTypes
+        , ConstraintKinds
+        , FlexibleContexts
+        ]

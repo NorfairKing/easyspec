8d0c39d99a10219d4c1dcd6d1133522f45d8c8a7
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2017-04-09 16:42:19 +0200

    Another super simple signature inference algorithm

--- easyspec/src/EasySpec/Discover/SignatureInference.hs
+++ easyspec/src/EasySpec/Discover/SignatureInference.hs
@@ -8,5 +8,19 @@ inferFullSignature ::
        [EasyId] -- ^ Focus functions
     -> [EasyId] -- ^ Full scope (can still contain the interesting functions as well)
     -> InferredSignature
-inferFullSignature focus scope =
-    InferredSignature {sigFocusIds = focus, sigBackgroundIds = scope \\ focus}
+inferFullSignature = inferAlg $ \focus scope -> scope \\ focus
+
+inferEmptySignature ::
+       [EasyId] -- ^ Focus functions
+    -> [EasyId] -- ^ Full scope (can still contain the interesting functions as well)
+    -> InferredSignature
+inferEmptySignature = inferAlg $ \_ _ -> []
+
+inferAlg ::
+       ([EasyId] -> [EasyId] -> [EasyId]) -- ^ Something that chooses the background ids.
+    -> [EasyId]
+    -> [EasyId]
+    -> InferredSignature
+inferAlg func focus scope =
+    let bgSigFuncs = func focus scope
+    in InferredSignature {sigFocusIds = focus, sigBackgroundIds = bgSigFuncs}

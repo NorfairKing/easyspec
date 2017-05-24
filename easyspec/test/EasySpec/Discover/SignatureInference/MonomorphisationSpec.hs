4a376f9f1930aa670bb8e71d8476eb33819e9fa6
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2017-05-24 07:14:48 +0200

    A failing test case

--- easyspec/test/EasySpec/Discover/SignatureInference/MonomorphisationSpec.hs
+++ easyspec/test/EasySpec/Discover/SignatureInference/MonomorphisationSpec.hs
@@ -14,10 +14,15 @@ import EasySpec.Discover.TH
 
 spec :: Spec
 spec =
-    describe "getKindedTyVars" $
-    it "works on these simple examples with kind * type variables" $ do
-        getKindedTyVars $(easyType "a -> String") `shouldBe`
-            ([(Ident () "a", KindStar ())], CxEmpty mempty)
-        getKindedTyVars $(easyType "a -> b") `shouldBe`
-            ( [(Ident () "a", KindStar ()), (Ident () "b", KindStar ())]
+    describe "getKindedTyVars" $ do
+        it "works on these simple examples with kind * type variables" $ do
+            getKindedTyVars $(easyType "a -> String") `shouldBe`
+                ([(Ident () "a", KindStar ())], CxEmpty mempty)
+            getKindedTyVars $(easyType "a -> b") `shouldBe`
+                ( [(Ident () "a", KindStar ()), (Ident () "b", KindStar ())]
+                , CxEmpty mempty)
+        it
+            "works on this example of a function that is polymorphic in the monad" $
+            getKindedTyVars $(easyType "Monad m => m Int") `shouldBe`
+            ( [(Ident () "m", KindFn () (KindStar ()) (KindStar ()))]
             , CxEmpty mempty)

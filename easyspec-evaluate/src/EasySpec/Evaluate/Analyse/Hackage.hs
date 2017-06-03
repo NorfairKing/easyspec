f9a560e66b70a55f1631a12efdf1ceb71039044a
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2017-06-03 06:13:38 +0200

    Fixed zift

--- easyspec-evaluate/src/EasySpec/Evaluate/Analyse/Hackage.hs
+++ easyspec-evaluate/src/EasySpec/Evaluate/Analyse/Hackage.hs
@@ -23,13 +23,7 @@ hackageRules = do
     hackageRule ~> need packageRules
 
 hackagePackages :: [(PackageName, [String], [String])]
-hackagePackages
-    -- [ $(makePackageTup "bytestring-0.10.8.1" [["Data", "ByteString"]])
- =
-    [ $(makePackageTup
-            "pretty-1.1.3.5"
-            [["Text", "PrettyPrint", "HughesPJClass"]])
-    ]
+hackagePackages = []
 
 hackageRulesFor :: (PackageName, [String], [String]) -> Rules String
 hackageRulesFor (package, _, _) = do

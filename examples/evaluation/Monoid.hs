bbbcec7882b471ec56ba54e8b0c97a2836a92bc2
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2017-06-05 09:41:18 +0200

    The monoid evaluation example

--- examples/evaluation/Monoid.hs
+++ examples/evaluation/Monoid.hs
@@ -1,7 +1,21 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
 module Monoid where
+
+import Data.List (sort, nub)
+import Prelude (Int, Num((+), (*), (-)), mod, head, Eq((==)))
 
 zero :: Int
 zero = 0
 
-combine :: Int -> Int -> Int
-combine i1 i2 = i1 + i2
+add :: Int -> Int -> Int
+add = (+)
+
+negate :: Int -> Int
+negate i = -i
+
+one :: Int
+one = 1
+
+multiply :: Int -> Int -> Int
+multiply = (*)

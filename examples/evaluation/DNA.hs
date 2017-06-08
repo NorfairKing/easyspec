eb37437b562a8d4757f7961e3537d04c0e822749
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2017-06-09 01:20:18 +0200

    larger scopes for examples

--- examples/evaluation/DNA.hs
+++ examples/evaluation/DNA.hs
@@ -3,10 +3,21 @@
 module DNA where
 
 import Data.Foldable (foldl')
-import Data.List (sort, lookup, elem, splitAt, reverse)
+import Data.List
+       (sort, lookup, elem, splitAt, reverse, uncons, intersperse,length,
+        intercalate, transpose, stripPrefix, group, inits, tails,
+        isPrefixOf, isSuffixOf, isInfixOf, partition, nub, delete, (\\),
+        union, intersect, sortOn, insert, isSubsequenceOf)
+import Data.Maybe (isJust, isNothing)
 import Prelude
-       (Int, Char, (/), (+), Maybe(..), fst, filter, (==), (/=), map,
-        take, drop, Double, Bool(..), zip, fromIntegral, length)
+       (Int, Num(..), Ord(..), div, mod, odd, even, Bool(..), Integer,
+        Float, Double, Rational, Word, Monoid(..), id, const, flip, ($),
+        take, drop, splitAt, takeWhile, dropWhile, span, break, lookup,
+        zip, zip3, zipWith, zipWith3, unzip, unzip3, scanl, scanl1, scanr,
+        scanr1, map, (++), filter, Bounded(..), Enum(..), words, lines,
+        unlines, unwords, Either(..), either, Ordering(..), Char, fst, snd,
+        curry, uncurry, Maybe(..), maybe, (&&), (||), not, head, Eq(..))
+
 
 {-# ANN module "HLint: ignore Use String" #-}
 
--- examples/evaluation/Monoid.hs
+++ examples/evaluation/Monoid.hs
@@ -2,10 +2,21 @@
 
 module Monoid where
 
-import Data.List (sort, nub)
+import Data.Foldable (foldl')
+import Data.List
+       (sort, lookup, elem, splitAt, reverse, uncons, intersperse,
+        intercalate, transpose, stripPrefix, group, inits, tails,
+        isPrefixOf, isSuffixOf, isInfixOf, partition, nub, delete, (\\),
+        union, intersect, sortOn, insert, isSubsequenceOf)
+import Data.Maybe (isJust, isNothing)
 import Prelude
-       (Int, Num((+), (*), (-)), Ord((<=), (<), (>), (>=)), div, mod, odd,
-        even, Bool(..), succ, pred, head, Eq((==), (/=)))
+       (Int, Num(..), Ord(..), div, mod, odd, even, Bool(..), Integer,
+        Float, Double, Rational, Word, Monoid(..), id, const, flip, ($),
+        take, drop, splitAt, takeWhile, dropWhile, span, break, lookup,
+        zip, zip3, zipWith, zipWith3, unzip, unzip3, scanl, scanl1, scanr,
+        scanr1, map, (++), filter, Bounded(..), Enum(..), words, lines,
+        unlines, unwords, Either(..), either, Ordering(..), Char, fst, snd,
+        curry, uncurry, Maybe(..), maybe, (&&), (||), not, head, Eq(..))
 
 zero :: Int
 zero = 0

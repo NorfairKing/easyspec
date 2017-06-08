7294b24f39513f0fd897f849403f0eb87372d7ec
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2017-06-09 01:25:32 +0200

    Woops

--- examples/evaluation/DNA.hs
+++ examples/evaluation/DNA.hs
@@ -10,7 +10,7 @@ import Data.List
         union, intersect, sortOn, insert, isSubsequenceOf)
 import Data.Maybe (isJust, isNothing)
 import Prelude
-       (Int, Num(..), Ord(..), div, mod, odd, even, Bool(..), Integer,
+       (Int, Num(..), Ord((<), (<=), (>), (>=), max, min), div, mod, odd, even, Bool(..), Integer,
         Float, Double, Rational, Word, Monoid(..), id, const, flip, ($),
         take, drop, splitAt, takeWhile, dropWhile, span, break, lookup,
         zip, zip3, zipWith, zipWith3, unzip, unzip3, scanl, scanl1, scanr,
